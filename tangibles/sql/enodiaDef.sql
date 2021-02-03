c7d2e384484672495f129faf0e70a6fd17cf7ce7
Brygg Ullmer <bullmer@BULLMERSTUDIO1.CAMPUS.CU.CLEMSON.EDU>
2021-02-02 23:55:10 -0500

    new table

--- tangibles/sql/enodiaDef.sql
+++ tangibles/sql/enodiaDef.sql
@@ -1,32 +1,40 @@
-# Initial population for Enodia tables
-# en prefix short for Enodia; enHt, for Enodia hextok
-# Initial definition by Brygg Ullmer
-# Begun 2021-02-02
+-- Initial population for Enodia tables
+-- en prefix short for Enodia; enHt, for Enodia hextok
+-- Initial definition by Brygg Ullmer
+-- Begun 2021-02-02
 
-create table enFacets {
-  id    int primary key,
+create table enFacets (
+  id    integer primary key,
   name  text,
-  descr text,
-};
+  descr text
+);
 
-create table enHtClass {
- id   int primary key,
- name text,
- descr text
-};
-
-create table enHtFacetClass {
- id    int primary key,
- class int, % references enHtClass.id
+create table enHtClass (
+ id    integer primary key,
  name  text,
  descr text
-};
+);
 
-create table enHtFacetInst {
-  id    int primary key,
-  facet int % references enHtFacetClass.id,
+create table enHtPerson (
+  id       integer primary key,
+  handle   text,
+  name     text,
+  uname    text,
+  groupId  text 
+);
+
+create table enHtFacetClass (
+ id    integer primary key,
+ class integer, -- references enHtClass.id
+ name  text,
+ descr text
+);
+
+create table enHtFacetInst (
+  id    integer primary key,
+  facet integer -- references enHtFacetClass.id,
   value text
-};
+);
 
-%%% end %%%
+-- end --
 
