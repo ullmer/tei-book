199f5626bc3a3d8a2912b0461cf5a119a80dfb15
U-CAMPUS\bullmer <bullmer@BULLMER-10-M.CAMPUS.CU.CLEMSON.EDU>
2021-02-23 11:20:17 -0500

    db progress

--- tangibles/sql/test-attach2.py
+++ tangibles/sql/test-attach2.py
@@ -4,27 +4,26 @@
 
 import yaml, sys
 
+scratchDBN = 'scratch.db3'
 childDBY = """
- countries: en-facet-countries.db3 
- states:    en-facet-states.db3 
- SDGs:      en-facet-unsdg.db3 
- students:  tei21-cu-students.db3""";
+ countries: common/en-facet-countries.db3 
+ states:    common/en-facet-states.db3 
+ SDGs:      common/en-facet-unsdg.db3 
+ students:  edu.clemson/tei21-cu-students.db3""";
 
-childDB = yaml.load(childDBY)
-print(childDB)
-sys.exit(-1)
+childDBs = yaml.load(childDBY) #both supports compactness
+print(childDBs)
 
 import sqlite3
-conn = sqlite3.connect('scratch.db3')
+conn = sqlite3.connect(scratchDBN)
 c = conn.cursor()
 
 for childDB in childDBs.keys():
   dbFn      = childDBs[childDB]
-  attachStr = 'attach database "{}" as {}'.format(dbFn, childDB)
+  attachStr = 'attach database "{}" as {};'.format(dbFn, childDB)
   print(attachStr)
-
-#c.execute('ATTACH DATABASE "db_1.sqlite" AS db_1')
-#c.execute('SELECT * FROM db_1.my_table')
+  c.execute(attachStr);
+  #c.execute('SELECT * FROM {}.my_table'.format(childDB)
 #conn.commit()
 #c.fetchall()
 
