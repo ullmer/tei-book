c4f9c57b144d13288f2cc46f745b256a5bf19644
Brygg Ullmer <bullmer@clemson.edu>
2021-02-24 00:06:16 -0500

    working toward country/student integration

---
+++ tangibles/sql/en-students-pop3.py
@@ -0,0 +1,60 @@
+# Brygg Ullmer, Clemson University
+# Begun 2021-02-03
+
+import enTableSql, sys, sqlite3
+
+fn  = 'tei21-cu-students.csv'
+dfn  = 'tei21-cu-students.db3'
+enTable = enTableSql.enTableSql(fn)
+#enTable.insertSqlRaw(dfn)
+
+#print(enTable.rows2)
+
+studIdxs = [2, 5, 3]; studrows = []; studentId = 1
+sdgIdxs  = [27, 28, 29, 30]; sdgrows = []
+
+
+
+#insert into enStudentFacet (id, symname) values (100, 'unsdg');
+
+for row in enTable.rows2:
+  studrow = [studentId]
+  for idx in studIdxs:
+    studrow.append(row[idx])
+  studrows.append(studrow)
+
+  for idx in sdgIdxs:
+    sdg = row[idx]
+    sdgrows.append([100, studentId, int(sdg)])
+  studentId+=1
+
+#print(studrows)
+#print(sdgrows)
+
+conn = sqlite3.connect(dfn)
+c = conn.cursor()
+
+#c.executemany(
+#   "insert into enTeiStudent (id, name, sgroup, ug) values (?,?,?,?)", 
+#    studrows)
+#conn.commit()
+
+c.execute("attach database 'en-facet-countries.db3' as countryDb;")
+c.execute("select id, abbrev from countryDB.enFacetCountries;")
+conn.commit()
+result = c.fetchall()
+#print(result)
+
+cabbrev2id = {}
+for el in result:
+  countryId, countryAbbrev = el
+  cabbrev2id[countryAbbrev] = countryId
+
+print(cabbrev2id.keys())
+
+#c.executemany(
+#  "insert into enStudentFacetRelation (facetId, studentId, facetVal) values (?,?,?)", 
+#   sdgrows)
+#conn.commit()
+
+#### end ###
