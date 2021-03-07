aa318aa47bad66c20aa21c4f1fb50e4f46bde148
Brygg Ullmer <bullmer@clemson.edu>
2021-03-07 15:07:15 -0500

    starting example

--- tangibles/redis/redis-ex04.py
+++ tangibles/redis/redis-ex04.py
@@ -3,30 +3,13 @@
 # Begun 2021-03-01
 
 import sys
-from rykTei21Base import *
+from rykTeiEx04 import *
+cyfn = 'rykTeiEx04.yaml' #commands yaml filename
+ryk = rykTeiEx04(cyfn)
 
-cyfn = 'tei21-commands.yaml' #commands yaml filename
-
-ryk = rykTei21Base(cyfn)
-
-print('Entering blocking keyboard loop')
+print('Entering blocking keyboard loop.  Press "h" for help=list of commands.')
 
 while True:
   ryk.procCh()
-
-sys.exit(-1)
-
-# take redis password as command-line argument
-
-if len(sys.argv) < 2:
-  print("redis hello-world error:")
-  print("redis passwd expected as command-line argument")
-  sys.exit(-1)
-
-host="redis-15905.c56.east-us.azure.cloud.redislabs.com"
-port="15905"
-pw  = sys.argv[1]
-
-r=redis.Redis(host=host, port=port, password=pw)
 
 ### end ###
