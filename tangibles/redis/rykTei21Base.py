4a5be8e6cfc1fef9bd17d07d95169ec4b680f366
Brygg Ullmer <bullmer@clemson.edu>
2021-03-07 13:53:33 -0500

    progress on yaml command load

---
+++ tangibles/redis/rykTei21Base.py
@@ -0,0 +1,26 @@
+###### Simple support code for TEI interaction supported by redis ###### 
+# Brygg Ullmer (Clemson U.) and xxx
+# Begun 2021-03-05
+
+import redis 
+import yaml
+import getch
+import sys
+import redYaKb
+
+##################### redis yaml keyboard class #####################
+
+class rykTei21Base(redYaKb):
+
+  def pushLED:
+  def pullNFCs:
+  def describeHW:
+  def help:
+  def specPhysical:
+  def specVirtual:
+  def specTextual:
+  def wait1:
+  def wait2:
+  def wait3:
+
+### end ###
