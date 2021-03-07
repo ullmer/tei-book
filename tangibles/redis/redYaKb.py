476d4f289408db78827116dd0a20af0754332608
Brygg Ullmer <bullmer@clemson.edu>
2021-03-07 12:55:13 -0500

    stub toward redis-yaml-keyboard class

---
+++ tangibles/redis/redYaKb.py
@@ -0,0 +1,14 @@
+###### Simple support code for TEI interaction supported by redis ###### 
+# Brygg Ullmer (Clemson U.) and xxx
+# Begun 2021-03-05
+
+import redis 
+import yaml
+import getch
+
+class redYaKb: #redis yaml keyboard 
+  def readCh(self):
+  def procCh(self, ch):
+
+
+#### end ###
