590847fb4a9515f206b11521b1e25ef8b6868be5
Brygg Ullmer <bullmer@clemson.edu>
2021-03-08 10:37:19 -0500

    simplifying led example

---
+++ tangibles/leds/ex05.py
@@ -0,0 +1,60 @@
+###### Simple support code for TEI interaction ###### 
+# By Millon McClelland, Brygg Ullmer, and TBD, Clemson University
+# Begun 2021-03-07
+
+from redYaKb import *
+from enLedColorLib import *
+import sys
+
+############################################################## 
+################## support callback functions ################
+
+class rykTeiEx04(redYaKb):
+
+  ##################### turn all LEDs red #####################
+  def allRed(self):    
+    self.selfDoc('allRed')
+    print('Millon, can you integrate the right calls here, please?')
+
+  ##################### turn all LEDs red #####################
+  def allGreen(self):  
+    self.selfDoc('allGreen')
+    print('Millon, can you integrate the right calls here, please?')
+
+  ##################### turn all LEDs red #####################
+  def allBlue(self):   
+    self.selfDoc('allBlue')
+    print('Millon, can you integrate the right calls here, please?')
+
+  ##################### turn all LEDs red #####################
+  def wait1(self):     
+    self.selfDoc('wait1')
+    sys.sleep(1)
+
+  ##################### turn all LEDs red #####################
+  def wait2(self):     
+    self.selfDoc('wait1')
+    sys.sleep(2)
+
+  ##################### turn all LEDs red #####################
+  def wait3(self):     
+    self.selfDoc('wait1')
+    sys.sleep(3)
+
+############################################################## 
+################## support callback functions ################
+
+def main():
+  elcl = enLedColorLib()
+  print(elcl.basecolorKeyHash.keys())
+  colorseq = elcl.getBasecolorSeq('oopoop')
+  print(colorseq)
+  scaledColor = elcl.mapSeqIntensity(colorseq, 'A9BAAA')
+  print(colorseq[0], scaledColor)
+
+if __name__ == "__main__":
+  main()
+
+### end ###
+
+### end ###
