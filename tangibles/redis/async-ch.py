7958c7272067535a4e739720923bf7ec4102e52c
Brygg Ullmer <ullmer@acm.org>
2021-04-10 16:51:54 -0400

    semi-async kb working

--- tangibles/redis/async-ch.py
+++ tangibles/redis/async-ch.py
@@ -5,20 +5,24 @@
 import sys
 import asyncio
 import getch
-#import aioredis # async I/O redis libraries
 #import concurrent.futures
 
 #https://docs.python.org/3/library/asyncio-eventloop.html
 
 def blocking_io():
-  ch = getch()
+  ch = getch.getch()
   print("<" + ch + ">")
 
 async def main():
   loop = asyncio.get_running_loop()
-  result = await loop.run_in_executor(None, blocking_io)
-  print('default thread pool', result)
+  while True:
+    await loop.run_in_executor(None, blocking_io)
 
-asyncio.run(main())
+#asyncio.run(main())
+
+loop = asyncio.get_event_loop()
+loop.create_task(main())
+loop.run_forever()
+loop.close()
 
 ### end ###
