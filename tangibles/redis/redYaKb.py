649b1b3d7d34e47f916c2eadab6196c632b34c7f
ullmer <ullmer@acm.org>
2021-04-11 00:53:22 -0400

    redis-ex09.py
    moving working redis codes into redYaKb

--- tangibles/redis/redYaKb.py
+++ tangibles/redis/redYaKb.py
@@ -10,9 +10,11 @@ from pynput import keyboard
 # Accessibility, and add "Terminal" to "Allow the apps below to control your 
 # computer" (if you are running Python or Python3 from Terminal)
 
-import redis 
-import yaml
 import sys
+import yaml
+import redis 
+import asyncio
+import aioredis 
 
 # python3 -m pip install redis pyyaml pynput
 
@@ -24,19 +26,28 @@ class redYaKb:
   commandHash      = None
   kbListener       = None
 
+  host = "redis-15905.c56.east-us.azure.cloud.redislabs.com"
+  port = "15905"
+  pw   = None
+  pool     = None  #redis handle
+  receiver = None
+  channels = None
+
   ##################### constructor ##################### 
 
-  def __init__(self, commandsYamlFn):
+  def __init__(self, commandsYamlFn, pw):
     self.ingestCommandYamlFn(commandsYamlFn)
     #self.listCommands()
     self.activateKeyListener()
+    self.pw = pw
+    self.channels = []
 
   ##################### key callbacks ##################### 
 
   def on_press(self, key):
     try:
       #print('alphanumeric key {0} pressed'.format(key.char))
-      self.procCh(ch=key)
+      self.procCh(ch=key.char)
     except AttributeError:
       print('special key {0} pressed'.format(key))
 
@@ -64,7 +75,7 @@ class redYaKb:
   def selfDoc(self, selfCmd):
     print("self documentation called for command " + selfCmd)
 
-  ##################### read + process file containing yaml command bindings #####################
+  ############## read + process file containing yaml command bindings #############
 
   def ingestCommandYamlFn(self, sourceYamlFn): 
     try:
@@ -130,23 +141,83 @@ class redYaKb:
   ##################### process character #####################
 
   def procCh(self, ch=None): #if none, will use readCh (blocking)
+    print("redYaKb procCh " + ch)
     if ch == None:
       ch = self.readCh()
 
-      if ch in self.commandHash:
-        commandDescr = self.commandHash[ch]
-        if 'command' in commandDescr:
-          commandText = commandDescr['command']
-          print(commandText)
-          cmd = self.getCmd(commandDescr)
-          try:
-            cmd()
-            return(True)
-          except:
-            print("redYaKb getCmd: problem with getattr " + commandText) 
-            e = sys.exc_info()   #e = sys.exc_info()[0]
-            print('error: '+str(e))
-            return False
+    if ch in self.commandHash:
+      commandDescr = self.commandHash[ch]
+      if 'command' in commandDescr:
+        commandText = commandDescr['command']
+        print(commandText)
+        cmd = self.getCmd(commandDescr)
+        try:
+          cmd()
+          return(True)
+        except:
+          print("redYaKb getCmd: problem with getattr " + commandText) 
+          e = sys.exc_info()   #e = sys.exc_info()[0]
+          print('error: '+str(e))
+          return False
 
-#### end ###
+  ##################### redis functions #####################
 
+
+  async def connect(self): 
+    #self.redHand = await aioredis.create_connection(address=(self.host, self.port), 
+    #                                                password=self.pw)
+    self.pool = await aioredis.create_redis_pool(address=(self.host, self.port), 
+                                                    password=self.pw)
+
+  async def testget(self): 
+    if self.pool == None:
+      print("redWrap error: redis pool not initiated");
+      return
+
+    result = await self.pool.execute('hgetall', 'teiDomains');
+    print(result)
+
+#################### set up redis publications ####################
+
+  async def pub(self, key, value): 
+    if self.pool == None:
+      print("redWrap error: redis pool not initiated");
+      return
+
+    await self.pool.publish(key, value)
+
+#################### handle message ####################
+
+  async def handle_msg(self, msg):
+    print('Received Message:', msg)
+
+#################### reader ####################
+  
+  async def reader(self, mpsc):  
+    async for channel, msg in mpsc.iter():
+      assert isinstance(channel, aioredis.AbcChannel)
+      print("Received {!r} in channel {!r}".format(msg, channel))
+
+  #https://aioredis.readthedocs.io/en/v1.3.1/mpsc.html
+  #https://docs.python.org/3/library/asyncio-task.html
+  #https://stackoverflow.com/questions/34118816/aioredis-and-pub-sub-arent-asnyc
+
+#################### set up redis subscriptions ####################
+
+  async def sub(self): 
+    if self.pool == None:
+      print("redWrap error: redis pool not initiated");
+      return
+
+    #self.receiver = aioredis.pubsub.Receiver(loop=loop)
+    self.receiver = aioredis.pubsub.Receiver()
+    asyncio.create_task(self.reader(self.receiver)) 
+    channelArgs = []
+    for channelArg in self.channels:
+      channelArgs.append(self.receiver.channel(channelArg))
+    await self.pool.subscribe(*channelArgs) 
+
+    #https://www.informit.com/articles/article.aspx?p=2979063&seqNum=8
+      
+
+### end ###
