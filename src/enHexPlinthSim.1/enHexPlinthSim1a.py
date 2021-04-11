6a529d4f02c909094d66d0de17945e680d1bc541
ullmer <ullmer@acm.org>
2021-04-11 14:15:52 -0400

    directory refactoring

--- src/enHexPlinthSim.1/enHexPlinthSim1a.py
+++ src/enHexPlinthSim.1/enHexPlinthSim1a.py
@@ -14,7 +14,7 @@ root   = Tk()
 canvas = Canvas(root, width=canvas_width, height=canvas_height)
 canvas.pack()
 
-img = PhotoImage(file="enHexPlinthSim02b.png")
+img = PhotoImage(file="images/enHexPlinthSim02b.png")
 #canvas.create_image(0, canvas_height, anchor=NW, image=img)
 canvas.create_image(0, 0, anchor=NW, image=img)
 
