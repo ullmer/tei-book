b5a92d2d1f8db7558523c0e51022fa68345ff4c8
ullmer <ullmer@acm.org>
2021-04-11 14:13:57 -0400

    first state for simulator

---
+++ src/enHexPlinthSim.1/enHexPlinthSim1a.py
@@ -0,0 +1,24 @@
+# Enodia Hex Plinth Simulator
+# By Brygg Ullmer and TBD, Clemson University
+# Begun 2021-04-11
+
+# Resources:
+# https://www.python-course.eu/tkinter_canvas.php
+
+from tkinter import *
+
+canvas_width  = 900
+canvas_height = 450
+
+root   = Tk()
+canvas = Canvas(root, width=canvas_width, height=canvas_height)
+canvas.pack()
+
+img = PhotoImage(file="enHexPlinthSim02b.png")
+#canvas.create_image(0, canvas_height, anchor=NW, image=img)
+canvas.create_image(0, 0, anchor=NW, image=img)
+
+mainloop()
+
+### end ###
+
