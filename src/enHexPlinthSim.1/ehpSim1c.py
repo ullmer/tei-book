faea16133a2c24a0d81ea79ad4226b19ec6455f4
ullmer <ullmer@acm.org>
2021-04-12 00:06:41 -0400

    periodic and tk seem to be co-running!  next step

--- src/enHexPlinthSim.1/ehpSim1c.py
+++ src/enHexPlinthSim.1/ehpSim1c.py
@@ -2,12 +2,14 @@
 # Brygg Ullmer and TBD, Clemson University
 # Begun 2021-04-11
 
+#https://www.reddit.com/r/Python/comments/33ecpl/neat_discovery_how_to_combine_asyncio_and_tkinter/
+
+#https://gist.github.com/nameoftherose/037e97b95d94df7867ee328ea8009857
+
 #https://stackoverflow.com/questions/47895765/use-asyncio-and-tkinter-or-another-gui-lib-together-without-freezing-the-gui
 
 from tkinter import *
 import asyncio
-import threading
-import random
 
 from enHexPlinthSim1c import *
 
@@ -19,43 +21,34 @@ async def periodic():
     # pynput threading and asyncio, absent this function, the event loop
     # isn't serviced for pubsub broadcasts. long sigh.
 
-def _asyncio_thread(async_loop):
-    async_loop.run_until_complete(do_urls())
-
-def do_tasks(async_loop):
-    """ Button-Event-Handler starting the asyncio part. """
-    threading.Thread(target=_asyncio_thread, args=(async_loop,)).start()
-
-async def one_url(url):
-    """ One task. """
-    sec = random.randint(1, 8)
-    await asyncio.sleep(sec)
-    return 'url: {}\tsec: {}'.format(url, sec)
-
-async def do_urls():
-    """ Creating and starting 10 tasks. """
-    tasks = [one_url(url) for url in range(10)]
-    completed, pending = await asyncio.wait(tasks)
-    results = [task.result() for task in completed]
-    print('\n'.join(results))
+async def run_tk(root, interval=0.1):  #original interval = 0.05
+  try:
+    while True:
+      root.update()
+      #yield from asyncio.sleep(interval)
+      await asyncio.sleep(interval)
+  except tkinter.TclError as e:
+    if "application has been destroyed" not in e.args[0]:
+      raise
 
 #################### main ####################
 
-def main(async_loop):
+async def main(async_loop):
   colorTeiCrim = '#851c16'
 
   ehps = enHexPlinthSim()
   ehps.buildGui()
   #ehps.changeLEDSimColor(5, colorTeiCrim)
   ehps.changeLEDSimColor(4, colorTeiCrim)
-  ehps.root.mainloop()
+  #ehps.root.mainloop()
+  await run_tk(ehps.root)
 
 if __name__ == '__main__':
-    async_loop = asyncio.get_event_loop()
-    async_loop.create_task(periodic())
-    async_loop.create_task(main(async_loop))
-    async_loop.run_forever()
-    asyn_loop.close()
+    aloop = asyncio.get_event_loop()
+    aloop.create_task(periodic())
+    aloop.create_task(main(aloop))
+    aloop.run_forever()
+    aloop.close()
 
     #main(async_loop)
 
