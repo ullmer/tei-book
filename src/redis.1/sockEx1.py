45bb61ee9f163631d5e9dae8c1932e52fa9ee315
ullmer <ullmer@acm.org>
2021-04-13 14:30:03 -0400

    socket example from stackoverflow response

---
+++ src/redis.1/sockEx1.py
@@ -0,0 +1,21 @@
+#https://stackoverflow.com/questions/48506460/python-simple-socket-client-server-using-asyncio
+
+import asyncio, socket
+
+async def handle_client(reader, writer):
+    request = None
+    while request != 'quit':
+        request = (await reader.read(255)).decode('utf8')
+        response = str(eval(request)) + '\n'
+        writer.write(response.encode('utf8'))
+        await writer.drain()
+    writer.close()
+
+async def run_server():
+    server = await asyncio.start_server(handle_client, 'localhost', 15555)
+    async with server:
+        await server.serve_forever()
+
+asyncio.run(run_server())
+
+### end ###
