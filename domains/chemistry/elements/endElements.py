2bb80757efea7563a2b47f8c91f23ded00534556
Brygg Ullmer <bullmer@clemson.edu>
2021-06-14 14:21:49 -0400

    listing elements and retrieve data by fullname implemented

--- domains/chemistry/elements/endElements.py
+++ domains/chemistry/elements/endElements.py
@@ -11,22 +11,48 @@ import json
 class endElements:
   elementUrl  = 'https://github.com/Bowserinator/Periodic-Table-JSON.git'
   elementJson = 'periodic-table-lookup.json'
-  elementData = None
+  elementData         = None
+  elementFullnames    = None
+  elementFullnameHash = None
 
   #################### load data ####################
 
   def loadData(self):
     elementF         = open(self.elementJson, 'r+t')
     self.elementData = json.load(elementF)
+
+    els = self.getElementList()
   
   #################### getElementList ####################
 
   def getElementList(self):
+    if self.elementFullnames != None:
+      return self.elementFullnames
+
     if self.elementData == None:
       self.loadData()
 
-    elementList = self.elementData["order"]
-    return elementList
+    self.elementFullnames = self.elementData["order"]
+    return self.elementFullnames
+
+  #################### getElement ####################
+
+  def getElementByFullname(self, elementFullname):
+    if self.elementData == None:
+      self.loadData()
+
+    if self.elementFullnameHash == None:
+      self.elementFullnameHash = {}
+
+    if elementFullname in self.elementFullnameHash:
+      result = self.elementFullnameHash[elementFullname]
+      return result
+
+    if elementFullname in self.elementData:
+      self.elementFullnameHash[elementFullname] = self.elementData[elementFullname]
+      return self.elementFullnameHash[elementFullname]
+
+    return None # error message would be preferable
 
 ############################################## 
 #################### main #################### 
@@ -34,6 +60,7 @@ class endElements:
 def main():
   ee = endElements()
   print(ee.getElementList())
+  print(ee.getElementByFullname('aluminium'))
 
 if __name__ == "__main__":
   main()
