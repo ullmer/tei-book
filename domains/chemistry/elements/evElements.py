609dfb00938640a9310edfa6bbb18a011cdba401
Brygg Ullmer <bullmer@clemson.edu>
2021-06-14 22:07:03 -0400

    stub for tkinter aspects

--- domains/chemistry/elements/evElements.py
+++ domains/chemistry/elements/evElements.py
@@ -2,19 +2,31 @@
 # Support class for engaging periodic table of the elements
 
 from edElements import *
+from tkinter    import *
 
 ######################################################################### 
 #################### Enodia Visual : Chemical Elements #################### 
 
 class evElements(edElements):
+  root = None
+
+  #################### build GUI #################### 
   def buildGui(self):
-    pass
+    self.root = Tk()
+    a = Label(self.root, text="Hello, world!")
+    a.pack()
+
+  #################### mainloop/event handler #################### 
+  def mainloop(self):
+    self.root.mainloop()
 
 ############################################## 
 #################### main #################### 
 
 def main():
   ev = evElements()
+  ev.buildGui()
+  ev.mainloop()
 
 if __name__ == "__main__":
   main()
