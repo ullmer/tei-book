07b71b4a13b6dae91dc09baf5a769bf9cf5a9917
Brygg Ullmer <bullmer@clemson.edu>
2021-06-14 22:17:19 -0400

    warming up to table cells

--- domains/chemistry/elements/evElements.py
+++ domains/chemistry/elements/evElements.py
@@ -9,21 +9,27 @@ from tkinter    import *
 
 class evElements(edElements):
   root = None
-  cellWidth  = 100
-  cellHeight = 100
+  cellWidth  = 5
+  cellHeight = 5
 
   #################### build GUI #################### 
   def buildGui(self):
     self.root = Tk()
-    cell = self.buildCell(self.root)
+    cell = self.buildCell(self.root, '1', 'H')
     cell.pack()
 
     #a = Label(self.root, text="Hello, world!")
     #a.pack()
 
   #################### mainloop/event handler #################### 
-  def buildCell(self, parentWidget):
+  def buildCell(self, parentWidget, label1, label2):
     cell = Frame(parentWidget, borderwidth = 1, width=self.cellWidth, height=self.cellHeight)
+    l1   = Label(cell, text=label1, width=self.cellWidth)
+    l2   = Label(cell, text=label2, width=self.cellWidth)
+    for label in [l1, l2]:
+      #label.pack(expand=1, fill=BOTH)
+      label.pack()
+
     return cell
 
   #################### mainloop/event handler #################### 
