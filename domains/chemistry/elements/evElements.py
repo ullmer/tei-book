3c6a2ddf8d2f12b42c85a46c7de40ce719d5ebd7
Brygg Ullmer <bullmer@clemson.edu>
2021-06-14 22:03:30 -0400

    stub for tkinter aspects

---
+++ domains/chemistry/elements/evElements.py
@@ -0,0 +1,23 @@
+# Brygg Ullmer, Miriam Konkel, and Breanna Filipiak
+# Support class for engaging periodic table of the elements
+
+from edElements import *
+
+######################################################################### 
+#################### Enodia Visual : Chemical Elements #################### 
+
+class evElements(edElements):
+  def buildGui(self):
+    pass
+
+############################################## 
+#################### main #################### 
+
+def main():
+  ev = evElements()
+
+if __name__ == "__main__":
+  main()
+
+### end ###
+
