a15b66a8a65d1325e2d1a7db12ca3673f93cccb3
Brygg Ullmer <ullmer@acm.org>
2021-12-17 09:43:23 -0500

    horizontal stripes for Enodia Fargate

---
+++ src/book-synth.1/figThumbnails/genTh02a.py
@@ -0,0 +1,46 @@
+# Generate book thumbnails
+# Brygg Ullmer, Clemson University
+# Begun 2021-03-19
+
+from PIL import Image, ImageDraw, ImageFont
+from imgSupport import *
+import sys
+
+basedir     = '/home/bullmer/book/2021-03-17b/'
+imageListFn = 'main12b.figs'
+tmpFn       = 'temp.jpg'
+imageListF  = open(imageListFn, 'r+t')
+rawlines    = imageListF.readlines()
+#outPrefix = 'thumbs/img' 
+outPrefix = 'enodiaFar/img'
+targetRes  = [900, 100] # .653" x 6" natively would conform 919 x 100
+#targetRes = [150, 300]
+#targetRes = [250,250]
+
+idx = 0
+for rawline in rawlines:
+  cleanline = rawline.rstrip()
+  extension = cleanline[-3:]
+  imgFn = basedir + cleanline
+
+  print('reading '+extension)
+  if extension == 'pdf':
+    print(imgFn)
+    worked = convPdf2Jpg1(imgFn, tmpFn)
+    if worked == False:
+      convPdf2Jpg2(imgFn, tmpFn)
+  else:
+    idx += 1
+    continue
+
+  outFn = outPrefix + str(idx).zfill(3) + '.jpg'
+  idx += 1
+  try:
+    resize_and_crop(tmpFn, outFn, targetRes, crop_type='middle')
+    pass
+  except:
+    print("problem opening image" + cleanline)
+    e = sys.exc_info()   #e = sys.exc_info()[0]
+    print('error: '+str(e))
+
+### end ###
